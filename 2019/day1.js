4e0094add01aec7ac4af86d39504266065e44ae8
s-mv <shreerangvaidya28@gmail.com>
2020-12-04 18:48:38 +0530

    2019 stuff

--- 2019/day1.js
+++ 2019/day1.js
@@ -1,20 +1,29 @@
 const fs = require('fs');
 
-console.log("Day 2");
+console.log("Day 1");
 
-fs.readFile('./input.day2.txt', 'utf8', (err, data) => {
+fs.readFile('./input.day1.txt', 'utf8', (err, data) => {
   if (err) {
     console.log("Oops, there's an error reading the input file:\n", err);
     return;
   }
   let arr = data.split('\n');
   let total = 0;
-  for(i in arr) {
-    if(~~arr[i] == 0) {
-      continue;
+  let total2 = 0;
+  let i = 0;
+  // for(i in arr) {
+    let p = ~~arr[i];
+    if(p == 0) {
+      // continue;
     }
-    total += Math.floor(~~arr[i] / 3) - 2;
-    console.log(~~arr[i], Math.floor(~~arr[i] / 3) - 2)
-  }
-  console.log('total needed', total);
+    total += Math.floor(p / 3) - 2;
+    while (p >= 0) {
+      total2 += p;
+      console.log(p)
+      console.log(' ', total2)
+      p = Math.floor(p / 3) - 2;
+    }
+  // }
+  console.log('total fuel needed:', total);
+  console.log('total fuel needed for fuel:', total2);
 });
