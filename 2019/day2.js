f628b96c0abcfe28dbf9786674df00b72f3fbdb7
s-mv <shreerangvaidya28@gmail.com>
2020-12-05 19:10:25 +0530

    refinements

---
+++ 2019/day2.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+console.log("Day 2");
+
+fs.readFile('./input.day2.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log("Oops, there's an error reading the input file:\n", err);
+    return;
+  }
+  let arr = data.split(',');
+  let final = 19690720;
+  function getNum(arr) {
+    let breakable = false;
+    for (let i = 0; i < arr.length; i += 4) {
+      let I = ~~arr[i], O = ~~arr[i + 3];
+      let j = ~~arr[i + 1], k = ~~arr[i + 2];
+      switch (I) {
+        case 1:
+          arr[O] = ~~(~~arr[j] + ~~arr[k]);
+          break;
+        case 2:
+          arr[O] = ~~(~~arr[j] * ~~arr[k]);
+          break;
+        case 99:
+          // console.log('terminating...', I);
+          breakable = true;
+          break;
+        default:
+          // console.log('(due to error)', I);
+          breakable = true;
+          break;
+      }
+      if (breakable) break;
+    }
+    return (arr[0]);
+  }
+  // checking for 19690720
+  for (let i = 0; i <= 99; i++) {
+    for (let j = 0; j <= 99; j++) {
+      let newArr = [...arr];
+      newArr[1] = i, newArr[2] = j;
+      if(getNum(newArr) == final) {
+        console.log('final noun:', i, '\nand verb:', j, '\n100 * noun + verb =', 100 * i + j);
+      }
+    }
+  }
+  arr[1] = 12, arr[2] = 2;
+  console.log(getNum(arr), 'is the first number.');
+});
--- 2020/day5.js
+++ 2020/day5.js
@@ -9,13 +9,10 @@ fs.readFile('./input.day5.txt', 'utf8', (err, data) => {
   }
   let arr = data.split('\n');
   let highest = 0;
-  let ranges = [];
   let IDs = [];
   for (const i in arr) {
     let p = arr[i];
-    if (arr[i] == '') {
-      continue;
-    }
+    if (arr[i] == '') { continue; }
     let rangex = [0, 127]
     let rangey = [0, 7]
     for (let k = 0; k < p.length; k++) {
@@ -38,15 +35,11 @@ fs.readFile('./input.day5.txt', 'utf8', (err, data) => {
     }
     // ranges.push({ x: rangex[0], y: rangey[0] });
     IDs.push(rangex[0] * 8 + rangey[0]);
-    if (rangex[0] * 8 + rangey[0] > highest) {
-      highest = rangex[0] * 8 + rangey[0]
-    }
+    (rangex[0] * 8 + rangey[0] > highest) && (highest = rangex[0] * 8 + rangey[0]);
   }
   IDs.sort(function (a, b) { return a - b });
   for (let i in IDs) {
-    if (IDs[i - 1] && IDs[i] - IDs[i - 1] != 1) {
-      console.log("your seat is", IDs[i] - 1);
-    }
+    (IDs[i - 1] && IDs[i] - IDs[i - 1] != 1) && (console.log("your seat is", IDs[i] - 1));
   }
   console.log(highest, "is the greatest ID.");
 });
